541a986276ac9e6b69dc8bb65efe7df741c318eb
lidamao <zichy@foxmail.com>
2023-11-05 22:57:57 +0800

    引入express

--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.write('Hello ~');
-  res.end();
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间',
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小',
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('你好');
 });
 
-server.listen(3000, () => {
-  console.log('服务已启动！');
+app.get('/posts', (req, res) => {
+  res.send(data);
+});
+
+app.get('/posts/:postId', (req, res) => {
+  console.log(req.params);
+
+  // 获取内容ID
+  const { postId } = req.params;
+
+  // 查找具体内容
+  const posts = data.filter((item) => item.id == postId);
+
+  // 做出响应
+  res.send(posts[0]);
+});
+
+app.listen(port, () => {
+  console.log('服务已启动! ');
 });
